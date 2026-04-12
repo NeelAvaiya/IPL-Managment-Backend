12e2b744c7a1ba9320986a626a39559d09557a11
NeelAvaiya <neelavaiya201@gmail.com>
2026-04-12 13:30:53 +0530

    file upload done

--- src/app.js
+++ src/app.js
@@ -3,13 +3,25 @@ import express from "express";
 import authRoute from "./modules/auth/auth.routes.js";
 import multer from "multer";
 import ApiResponse from "./common/utils/api-response.js";
+import path from "path";
 
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-const upload = multer();
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'public/uploads')
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
+    const ext = path.extname(file.originalname)
+    cb(null, file.fieldname + '-' + uniqueSuffix + ext)
+  }
+})
+
+const upload = multer({storage});
 
 app.post("/upload", upload.single("file"), (req, res) => {
     console.log(req.file);
