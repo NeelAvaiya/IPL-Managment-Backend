b84246954575d1020f22da3f9463e0a8a53ce17c
NeelAvaiya <neelavaiya201@gmail.com>
2026-04-15 22:04:37 +0530

    services created

---
+++ src/modules/ipl-ms/services/owner.service.js
@@ -0,0 +1,49 @@
+import ApiError from "../../../common/utils/api-error.js"
+import Owner from "../models/owner.model.js"
+
+const creteOwner = async({name, company}) =>{
+    const owner = await Owner.create({name, company});
+    return owner;
+}
+
+const getAllOwners = async()=>{
+    const owners = await Owner.find();
+
+    return owners;
+}
+
+const getOwnerById = async() =>{
+    const owner = await Owner.findById(id);
+
+    if(!owner){
+        throw ApiError.notfound("Owner not found")
+    };
+
+    return owner
+}
+
+const updateOwner = async(id, {name, company}) => {
+    const owner = await Owner.findByIdAndUpdate(
+        id,
+        {name, company},
+        {new: true, runValidators: true}
+    );
+
+    if(!owner){
+        throw ApiError.notfound("Owner not found")
+    };
+
+    return owner;
+}
+
+const deleteOwner = async(id, {name, company}) => {
+    const owner = await Owner.findByIdAndDelete(id);
+
+    if(!owner){
+        throw ApiError.notfound("Owner not found")
+    };
+
+    return owner;
+}
+
+export {creteOwner, getAllOwners, getOwnerById, updateOwner, deleteOwner};
