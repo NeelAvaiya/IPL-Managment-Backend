7914958c4ec7c421d8ad7d6676113a353ca8e45b
NeelAvaiya <neelavaiya201@gmail.com>
2026-04-15 22:17:33 +0530

    controller complete

--- src/modules/ipl-ms/controllers/owner.controller.js
+++ src/modules/ipl-ms/controllers/owner.controller.js
@@ -1,12 +1,31 @@
-const createOwner = async(req, res)=>{};
+import * as ownerService from "../services/owner.service.js";
+import ApiResponse from "../../../common/utils/api-response.js"
 
-const getAllOwners = async(req, res)=>{};
+const createOwner = async(req, res)=>{
+    const owner = await ownerService.creteOwner(req.body);
+    ApiResponse.created(res, "owner created successfully", owner);
+};
 
-const getOwnerById = async(req, res)=>{};
+const getAllOwners = async(req, res)=>{
+    const owners = await ownerService.getAllOwners();
+    ApiResponse.ok(res, "Owner fetched successfully", owners)
+};
 
-const updateOwner = async(req, res)=>{};
+const getOwnerById = async(req, res)=>{
 
-const deleteOwner = async(req, res)=>{};
+    const owner = await ownerService.getOwnerById(req.params.id);
+    ApiResponse.ok(res, "Owner fetched successfully", owner)
+};
+
+const updateOwner = async(req, res)=>{
+    const updatedOwner = await ownerService.updateOwner(req.params.id, req.body);
+    ApiResponse.ok(res, "Owner updated successfully", owner)
+};
+
+const deleteOwner = async(req, res)=>{
+    await ownerService.deleteOwner(req.params.id);
+    ApiResponse.ok(res, "Owner updated successfully")
+};
 
 export{
     createOwner,
